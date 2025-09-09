0d064ed194716261807d4e6a8ae698a6f0fc180d
Gog_2 <gog100500@gmail.com>
2025-09-09 10:11:43 +0300

    Main

---
+++ NewC#/Assets/Main/Script/Lesson1/ChangeColor.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    //[SerializeField] private Spawner m_Spawner;
+    [SerializeField] private float _speed =1f;
+    private Cube _SpawmdeCube;
+
+    private void Start()
+    {
+        //_SpawmdeCube = Spawner.SpawnCube(m_Spawner);
+    }
+
+}
---
+++ NewC#/Assets/Main/Script/Lesson1/Spawner.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class Spawner
+{
+    public static void SpawnCube(Vector3 position)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+    }
+}
